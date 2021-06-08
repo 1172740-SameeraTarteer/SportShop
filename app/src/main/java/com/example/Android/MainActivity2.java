4d4166ba4de243ec24080c62e7003a74a22e5a3b
bisan <bisantawafsha6@gmail.com>
2021-06-09 02:37:11 +0300

    project2

--- app/src/main/java/com/example/Android/MainActivity2.java
+++ app/src/main/java/com/example/Android/MainActivity2.java
@@ -2,49 +2,156 @@ package com.example.Android;
 
 import android.content.Intent;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class MainActivity2 extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity2 extends AppCompatActivity {
+    RadioGroup mRadioGroup;
+    int id;
+    String[] colorList;
+    String[] sizeList;
+    Items items;
+    String selectedColor = null;
+    String selectedSize = null;
+    int selectedId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
         Intent intent = getIntent();
-        int id = (int)intent.getExtras().get("item_id");
+         id = (int)intent.getExtras().get("item_id");
 
-        Items items = Items.Items[id];
-
-
+         items = Items.Items[id];
 
         TextView type= findViewById(R.id.edtType);
         TextView name= findViewById(R.id.edtName);
         TextView desc= findViewById(R.id.edtDesc);
-        TextView color= findViewById(R.id.edtColor);
-        TextView size= findViewById(R.id.edtSize);
-
-
-
         name.setText(items.getName());
         type.setText(items.getType());
         desc.setText(items.getDescription());
-        color.setText(items.getColor());
-        size.setText(items.getSize());
 
-//        year.setText(String.valueOf(items.getYear()));
+        ///To add Color & sizes choices
+        addRadioButtons();
 
     }
-    public void btnBack(View view){
-        Intent intent = new Intent(view.getContext(),
-                MainActivity.class);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.shoppinglist, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.listId:
+                Intent intent = new Intent(this,
+                        ShoppingList.class);
+
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    public void addRadioButtons() {
+        colorList = items.getColor().split(",");
+        sizeList = items.getSize().split(",");
+        final RadioGroup rgColor, rgSize;
+
+
+        rgColor = findViewById(R.id.rGColor);
+        ArrayList<RadioButton> listRadioButtonC = new ArrayList<>();
+        rgColor.setOrientation(RadioGroup.HORIZONTAL);// or RadioGroup.VERTICAL
+        for (int i = 0; i < colorList.length; i++) {
+            RadioButton rb = new RadioButton(this);
+            rb.setId(i);
+            rb.setText(colorList[i] + "");
+            listRadioButtonC.add(rb);
+            rgColor.addView(listRadioButtonC.get(i));
+        }
+        rgSize = findViewById(R.id.rGSize);
+        ArrayList<RadioButton> listRadioButtonS = new ArrayList<>();
+        rgColor.setOrientation(RadioGroup.HORIZONTAL);// or RadioGroup.VERTICAL
+        for (int i = 0; i < sizeList.length; i++) {
+            RadioButton rb = new RadioButton(this);
+            rb.setText(sizeList[i] + "");
+            rb.setId(i);
+            listRadioButtonS.add(rb);
+            rgSize.addView(listRadioButtonS.get(i));
+        }
+
+
+        rgColor.setOnCheckedChangeListener((rGroup, checkedId) -> {
+
+            int radioButtonID = rgColor.getCheckedRadioButtonId();
+            View radioButton = rgColor.findViewById(radioButtonID);
+            int idx = rgColor.indexOfChild(radioButton);
+            RadioButton r = (RadioButton) rgColor.getChildAt(idx);
+            selectedColor = r.getText().toString();
+        });
+        rgSize.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup rGroup, int checkedId) {
+
+                int radioButtonID = rgSize.getCheckedRadioButtonId();
+                View radioButton = rgSize.findViewById(radioButtonID);
+                int idx = rgSize.indexOfChild(radioButton);
+                RadioButton r = (RadioButton) rgSize.getChildAt(idx);
+                selectedSize = r.getText().toString();
+            }
+        });
+         selectedId = rgSize.getCheckedRadioButtonId();
+
+//        selectItem(selectedId,selectedColor, selectedSize);
+
+    }
+
+    public void selectItem(int selectedId,String selectedColor,String selectedSize){
+                    Intent intent = new Intent(this,
+                    ShoppingList.class);
+        intent.putExtra("selectedId",selectedId);
+        intent.putExtra("color",selectedColor);
+        intent.putExtra("size", selectedSize);
         startActivity(intent);
 
     }
+
+
+
+    public void btnBack(View view){
+        Intent intent = new Intent(view.getContext(),
+                ItemsRecycle.class);
+
+        view.getContext().startActivity(intent);
+
+    }
+
+
+
+    public void btnShopCart(View view){
+//        Intent intent = new Intent(view.getContext(),
+//                ShoppingList.class);
+//        view.getContext().startActivity(intent);
+
+    }
+
 }
 
 
